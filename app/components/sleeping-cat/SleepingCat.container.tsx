ba7599a27031331581472dee46db8e01fddf208a
sadkingo <hker0004@gmail.com>
2024-12-09 20:39:17 +0100

    Created component for the lazy cat

---
+++ app/components/sleeping-cat/SleepingCat.container.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SleepingCat from "./SleepingCat.component";
+
+const SleepingCatContainer = () => {
+  return <SleepingCat />;
+};
+
+export default SleepingCatContainer;
