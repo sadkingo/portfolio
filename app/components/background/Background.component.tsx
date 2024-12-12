a26fcb513f6c5693ee4c316475fdc81066d1f7ce
sadkingo <hker0004@gmail.com>
2024-12-13 00:27:46 +0100

    Added moon starts and sun , renamed the images folder

--- app/components/background/Background.component.tsx
+++ app/components/background/Background.component.tsx
@@ -1,22 +1,27 @@
 import React from "react";
-import Lay1 from "@images/background-layers/lay1.svg";
-import Lay2 from "@images/background-layers/lay2.svg";
-import Lay3 from "@images/background-layers/lay3.svg";
-import Lay4 from "@images/background-layers/lay4.svg";
-import Lay5 from "@images/background-layers/lay5.svg";
-import Lay6 from "@images/background-layers/lay6.svg";
-import Lay7 from "@images/background-layers/lay7.svg";
+import Sun from "@images/background/lay1.svg";
+import Lay2 from "@images/background/lay2.svg";
+import Lay3 from "@images/background/lay3.svg";
+import Lay4 from "@images/background/lay4.svg";
+import Lay5 from "@images/background/lay5.svg";
+import Lay6 from "@images/background/lay6.svg";
+import Lay7 from "@images/background/lay7.svg";
+import Moon from "@images/background/Moon.svg";
+import Stars from "@images/background/stars.svg";
+
 const Background = () => {
   return (
-    <div className="absolute flex w-full h-full -bottom-10">
+    <div className="absolute flex w-full h-full -bottom-10 -z-1">
       <div className="relative flex justify-center w-full">
-        <Lay1 className="absolute w-2/3 m-auto -top-10 nimate-slide-in-top"></Lay1>
+        <Stars className="absolute w-full -top-10 dark:animate-slide-in-top animate-slide-out-top"/>
+        <Moon className="absolute w-96 -top-10 dark:animate-slide-in-top animate-slide-out-top"/>
+        <Sun className="absolute w-fit -top-10 dark:animate-slide-out-top dark:opacity-0 animate-slide-in-top"/>
         <Lay2 className="absolute w-full h-full animate-slide-in-bottom animation-delay-100" />
-        <Lay3 className="absolute w-full h-full animate-slide-in-bottom animation-delay-200"></Lay3>
-        <Lay4 className="absolute w-full h-full animate-slide-in-bottom animation-delay-400"></Lay4>
-        <Lay5 className="absolute w-full h-full animate-slide-in-bottom animation-delay-600"></Lay5>
-        <Lay6 className="absolute w-full h-full animate-slide-in-bottom animation-delay-700"></Lay6>
-        <Lay7 className="absolute w-full h-full animate-slide-in-bottom animation-delay-750"></Lay7>
+        <Lay3 className="absolute w-full h-full animate-slide-in-bottom animation-delay-200" />
+        <Lay4 className="absolute w-full h-full animate-slide-in-bottom animation-delay-400" />
+        <Lay5 className="absolute w-full h-full animate-slide-in-bottom animation-delay-600" />
+        <Lay6 className="absolute w-full h-full animate-slide-in-bottom animation-delay-700" />
+        <Lay7 className="absolute w-full h-full animate-slide-in-bottom animation-delay-750" />
       </div>
     </div>
   );
