ea01bb91a257e236367659466a60f92fa9bbeb6e
sadkingo <hker0004@gmail.com>
2024-12-14 15:18:09 +0100

    Adjusted animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,14 +15,20 @@ export default {
     extend: {
       animation: {
         "slide-in-top":
-          "slide-in-top 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both",
+          "slide-in-top 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) both",
         "slide-in-bottom":
           "slide-in-bottom 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both",
+        "slide-out-top":
+          "slide-out-top 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) both",
       },
       keyframes: {
         "slide-in-top": {
           "0%": { transform: "translateY(-1000px)", opacity: "0" },
           "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "slide-out-top": {
+          "0%": { transform: "translateY(0)", opacity: "1" },
+          "100%": { transform: "translateY(-1000px)", opacity: "0" },
         },
         "slide-in-bottom": {
           "0%": { transform: "translateY(1000px)", opacity: "0" },
@@ -35,6 +41,10 @@ export default {
         "750": "750ms",
         "850": "850ms",
         "900": "900ms",
+      },
+      transitionDuration:{
+        "2000": "2000ms",
+        "5000": "5000ms"
       },
       colors: {
         background: "var(--background)",
