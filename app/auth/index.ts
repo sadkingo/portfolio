79fcba7d0582107448221db5e33f92013b42ad91
sadkingo <hker0004@gmail.com>
2024-12-17 00:44:08 +0100

    Added github OAuth

---
+++ app/auth/index.ts
@@ -0,0 +1,17 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const BASE_LOGIN_PATH = "/api/auth";
+
+const authOptions: NextAuthConfig = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  basePath: BASE_LOGIN_PATH,
+  secret: process.env.AUTH_SECRET,
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
