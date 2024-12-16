eedf15e33fe554cf60dbca82536888f4d4d95171
sadkingo <hker0004@gmail.com>
2024-12-17 00:46:20 +0100

    Created Main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
+import Content from "@components/content";
+import Profile from "@components/profile";
+import Tab from "@components/tab";
+import About from "@components/about";
+import Guestbook from "./components/guestbook";
 
-export default function Home() {
+
+export default async function Home() {
   return (
-    <>
-      <main>
-      </main>
-      <footer>
-      </footer>
-    </>
+    <div className="flex w-full h-full gap-5 my-16 max-md:mt-16 max-md:m-2 main-container max-md:flex-col md:px-5 ">
+      <Profile />
+      <Content>
+        <Tab
+          className="text-white "
+          tabClassName="text-gray-400"
+          defaultChecked
+          title={"About"}
+        >
+          <About />
+        </Tab>
+        <Tab className="text-white" title={"Guestbook"}>
+          <Guestbook />
+        </Tab>
+        <Tab className="text-white" title={"Contact"}>
+          contact
+        </Tab>
+      </Content>
+    </div>
   );
 }
