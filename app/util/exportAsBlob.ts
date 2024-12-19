da24d10c075319382981a92de6463638dec2a05a
sadkingo <hker0004@gmail.com>
2024-12-19 12:46:48 +0100

    Created util to export from tldraw as blob

---
+++ app/util/exportAsBlob.ts
@@ -0,0 +1,32 @@
+import { Box, Editor, exportToBlob } from "tldraw";
+
+interface Props {
+  editor: Editor;
+  container: HTMLElement;
+}
+async function exportAsBlob({ editor, container }: Props) {
+  const editorContainerComputedStyle = getComputedStyle(container);
+  const editorWidth = Math.min(
+    parseInt(editorContainerComputedStyle.width),
+    800,
+  );
+  const editorHeight = Math.min(
+    parseInt(editorContainerComputedStyle.height),
+    400,
+  );
+
+  const bounds = new Box(-70, -8, editorWidth, editorHeight);
+
+  const img = await exportToBlob({
+    editor: editor,
+    ids: [],
+    format: "png",
+    opts: {
+      padding: 0,
+      bounds,
+    },
+  });
+  return URL.createObjectURL(img);
+}
+
+export default exportAsBlob;
