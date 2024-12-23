5d17d16795608f6820e02c52001a39239b24da74
sadkingo <hker0004@gmail.com>
2024-12-23 21:09:20 +0100

    Cleaned

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 import NavBar from "./components/up-arrow/UpArrow";
 import Background from "./components/background";
