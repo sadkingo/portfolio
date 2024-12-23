07b45e343fd97c62f4fc71f5a588b69332e21c05
sadkingo <hker0004@gmail.com>
2024-12-23 21:06:32 +0100

    Adjusted guestbook logic

--- app/components/guestbook/Guestbook.component.tsx
+++ app/components/guestbook/Guestbook.component.tsx
@@ -1,37 +1,60 @@
 import React from "react";
-import Button from "../button";
 import { signIn, signOut } from "next-auth/react";
 import { Icon } from "@iconify/react";
 
-async function Guestbook({ currentUser }) {
-  return (
-    <div className="guestbook">{currentUser ? renderSignOutButton() : renderLoginButton()}</div>
-  );
+import Button from "../button";
+import { Comment, Image, User } from "@prisma/client";
+import Comments from "../comments";
 
-  function renderLoginButton() {
+interface CommentWithImage extends Comment {
+  image: Image;
+}
+interface Props {
+  currentUser: User | null;
+  comments: CommentWithImage[] | null;
+}
+
+function Guestbook({ currentUser, comments }: Readonly<Props>) {
+  return (
+    <div className="guestbook flex flex-col items-center justify-center gap-3">
+      {currentUser ? renderSignedIn() : renderSignedOut()}
+      <Comments comments={comments} />
+    </div>
+  );
+  function renderSignedIn() {
+    if (currentUser) {
+      return (
+        <div className="flex flex-col items-center">
+          <span className="text-white text-xl"> Welcome </span>
+          <div className="m-auto mb-2 [text-shadow:_0_4px_8px_rgba(14_165_223_/_0.5)] dark:text-sky-400 text-xl md:text-2xl leading-snug font-extrabold">
+            {currentUser?.name}
+          </div>
+          <div className="flex gap-3">{renderSignOutBtn()}</div>
+        </div>
+      );
+    }
+  }
+  function renderSignOutBtn() {
     return (
       <Button
         className="flex h-12 p-3 rounded-md shadow-md bg-amber-500 dark:bg-blue-900"
+        clickEvent={signOut}
+        text="Sign out"
+      >
+        <Icon className="h-full w-8" icon="material-symbols:logout" />
+      </Button>
+    );
+  }
+  function renderSignedOut() {
+    return (
+      <Button
+        className="flex h-12 w-fit p-3 rounded-md shadow-md bg-amber-500 dark:bg-blue-900"
         clickEvent={signIn}
         text={"Login with github"}
       >
         <Icon className="w-8 h-full" icon="mdi:github" />
       </Button>
     );
-  }
-  function renderSignOutButton() {
-    if (currentUser) {
-      return (
-        <div>
-          Welcome {currentUser.user?.name}
-          <Button
-            className="flex h-12 p-3 rounded-md shadow-md bg-amber-500 dark:bg-blue-900"
-            clickEvent={signOut}
-            text="Sign out"
-          ></Button>
-        </div>
-      );
-    }
   }
 }
 
--- app/components/guestbook/Guestbook.container.tsx
+++ app/components/guestbook/Guestbook.container.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import { auth } from "@/auth";
 
 import Guestbook from "./Guestbook.component";
-
+import { Prisma, User } from "@prisma/client";
+import { createUser, getUser } from "@/query/UserQuery";
+import { getPagedComments } from "@/query/CommentQuery";
 
 async function GuestbookContainer() {
-  const currentUser = await auth();
- 
+  let currentUser: User | null = null;
+  const oAuthUser = await auth();
+  if (oAuthUser) {
+    const oAuthUserEmail = oAuthUser.user!.email!;
+    currentUser = await getUser({ email: oAuthUserEmail });
+    if (currentUser === null) {
+      currentUser = await createUser(oAuthUser?.user as Prisma.UserCreateInput);
+    }
+  }
 
-  return <Guestbook currentUser={currentUser} />;
+  const comments = await getPagedComments();
+  return <Guestbook currentUser={currentUser} comments={comments} />;
 }
 
 export default GuestbookContainer;
