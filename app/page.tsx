f8d7d7ef6abc97559abf84736b312d18eb5beee4
sadkingo <hker0004@gmail.com>
2024-12-23 21:09:51 +0100

    Cleaned

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,18 @@ import Tab from "@components/tab";
 import About from "@components/about";
 import Guestbook from "./components/guestbook";
 
-
 export default async function Home() {
   return (
     <div className="flex w-full h-full gap-5 my-16 max-md:mt-16 max-md:m-2 main-container max-md:flex-col md:px-5 ">
       <Profile />
       <Content>
-        <Tab
-          className="text-white "
-          tabClassName="text-gray-400"
-          defaultChecked
-          title={"About"}
-        >
+        <Tab tabClassName="text-gray-400" defaultChecked title={"About"}>
           <About />
         </Tab>
-        <Tab className="text-white" title={"Guestbook"}>
+        <Tab title={"Guestbook"}>
           <Guestbook />
         </Tab>
-        <Tab className="text-white" title={"Contact"}>
-          contact
-        </Tab>
+        <Tab title={"My work"}>contact</Tab>
       </Content>
     </div>
   );
