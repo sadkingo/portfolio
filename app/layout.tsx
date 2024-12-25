1c9a366f591f80cacc760ff791ab42e8edaa7e61
sadkingo <hker0004@gmail.com>
2024-12-25 18:48:09 +0100

    Fix names

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-import NavBar from "./components/up-arrow/UpArrow";
+import UpArrow from "./components/up-arrow/UpArrow";
 import Background from "./components/background";
 
 const inter = Inter({
@@ -32,7 +32,7 @@ export default function RootLayout({
         className={`${inter.className} flex flex-col h-screen bg-slate-300 dark:bg-inherit antialiased md:overflow-hidden selection:bg-amber-600 dark:selection:bg-purple-500`}
       >
         <Background />
-        <NavBar />
+        <UpArrow />
         <main
           id="main"
           className="relative 2xl:px-36 mb-6 max-md:pb-16 max-md:h-fit h-5/6 w-full flex justify-center"
